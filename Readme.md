9c957733b91f505d20b307da457318dfe389d8bb
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# DocumentPreviewControl - How to override the Export command 
+
+
+<p>This example demonstrates how to override commands in the <a href="https://documentation.devexpress.com/#WPF/CustomDocument9697">Document Preview</a> (<a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfPrintingDocumentPreviewControltopic">DocumentPreviewControl</a>). Create a DocumentPreviewControl descendant and override the required (e.g., Export) methods.<br><strong><br>See also</strong><strong>:<br></strong><a href="https://www.devexpress.com/Support/Center/p/E4631">DocumentPreviewControl - How to override the Print and PrintDirect (Quick Print) commands</a> <strong><br></strong><a href="https://www.devexpress.com/Support/Center/p/E2974">How to remove standard bar items and add custom ones to the DocumentPreview toolbar</a></p>
+
+<br/>
+
+
---
+++ VB/MinimalisticReportPreviewDemo/MyDocumentPreviewControl.vb
@@ -0,0 +1,38 @@
+﻿Imports DevExpress.Xpf.Printing
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports DevExpress.XtraPrinting
+Imports DevExpress.Xpf.Printing.PreviewControl
+Imports System.IO
+Imports DevExpress.XtraPrinting.Native.ExportOptionsControllers
+
+Namespace MinimalisticReportPreviewDemo
+    Public Class MyDocumentPreviewControl
+        Inherits DocumentPreviewControl
+
+        Public Overrides Sub Export(ByVal format? As ExportFormat)
+            If format.HasValue AndAlso format.Value = ExportFormat.Xls Then
+                Dim optionsModel = ExportOptionsViewModel.Create(Document.PrintingSystem, format.Value)
+
+                optionsModel.OpenFileAfterExport = False
+                optionsModel.FileName = "test.xls"
+
+                CType(New DocumentPublishEngine(Document.PrintingSystem), DocumentPublishEngine).Export(optionsModel)
+                System.Windows.MessageBox.Show("!")
+                System.Diagnostics.Process.Start("test.xls")
+            Else
+                MyBase.Export(format)
+            End If
+        End Sub
+
+        Protected Overrides Sub Export(ByVal exportOptionsModel As ExportOptionsViewModel)
+            If String.IsNullOrEmpty(Path.GetExtension(exportOptionsModel.FileName)) Then
+                Dim extension = ExportOptionsControllerBase.GetControllerByOptions(exportOptionsModel.ExportOptions).GetFileExtension(exportOptionsModel.ExportOptions)
+                exportOptionsModel.FileName += extension
+            End If
+            MyBase.Export(exportOptionsModel)
+        End Sub
+    End Class
+End Namespace
