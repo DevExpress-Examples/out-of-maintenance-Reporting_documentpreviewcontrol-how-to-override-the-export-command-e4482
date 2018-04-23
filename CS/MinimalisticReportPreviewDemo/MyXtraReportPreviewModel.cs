12582acffb4cbcba1bc183dc65c25bb8d29e2f93
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MinimalisticReportPreviewDemo/MyXtraReportPreviewModel.cs
@@ -0,0 +1,19 @@
+﻿using DevExpress.Xpf.Printing;
+using DevExpress.XtraReports.UI;
+
+namespace MinimalisticReportPreviewDemo {
+    public class MyXtraReportPreviewModel : XtraReportPreviewModel {
+        public MyXtraReportPreviewModel(XtraReport report) : base(report) { }
+
+        protected override void Export(object parameter) {
+            if(parameter != null) {
+                if(parameter.ToString() == "Xls") {
+                    PrintingSystem.ExportToXls("test.xls");
+                    System.Windows.MessageBox.Show("!");
+                    System.Diagnostics.Process.Start("test.xls");
+                } else
+                    base.Export(parameter);
+            }
+        }
+    }
+}
---
+++ VB/MinimalisticReportPreviewDemo/MyXtraReportPreviewModel.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.Xpf.Printing
+Imports DevExpress.XtraReports.UI
+
+Namespace MinimalisticReportPreviewDemo
+	Public Class MyXtraReportPreviewModel
+		Inherits XtraReportPreviewModel
+		Public Sub New(ByVal report As XtraReport)
+			MyBase.New(report)
+		End Sub
+
+		Protected Overrides Sub Export(ByVal parameter As Object)
+			If parameter IsNot Nothing Then
+				If parameter.ToString() = "Xls" Then
+					PrintingSystem.ExportToXls("test.xls")
+					System.Windows.MessageBox.Show("!")
+					System.Diagnostics.Process.Start("test.xls")
+				Else
+					MyBase.Export(parameter)
+				End If
+			End If
+		End Sub
+	End Class
+End Namespace
